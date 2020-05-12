038c16eff4fdc5dba2a3c34deb2d4f0735784d96
Akash Gupta <akashgp9@gmail.com>
2020-05-12 21:13:22 +0530

    Added src folder

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
+//import Business from "../Business/Business";
+function App() {
+  return (
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+  );
+}
+
+export default App;
